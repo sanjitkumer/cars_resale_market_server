825f5b72b10cdfe938edf726702be2893d9666b3
sanjitkumer <sanjitk666@gmail.com>
2022-11-24 20:15:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', async(req, res) =>{
+    res.send('cars resale market server is running')
+})
+
+app.listen(port, () => console.log(`cars resale market running on ${port}`));
